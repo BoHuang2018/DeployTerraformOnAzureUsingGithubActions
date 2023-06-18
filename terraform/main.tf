60dda3b6d8bd7fd099840fab2a77c5f645de8c86
BoHuang2018 <tigerhuangbo@icloud.com>
2023-06-18 16:40:34 +0200

    use 'use_oidc' to azurerm

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,13 @@ terraform {
     storage_account_name = "tfstate19015"       # value of AZURE_STORAGE_ACCOUNT_FOR_TERRAFORM_STATE in ../.env
     container_name       = "tfstatebc"          # value of AZURE_BLOB_CONTAINER_NAME_FOR_TERRAFORM_STATE in ../.env
     key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
