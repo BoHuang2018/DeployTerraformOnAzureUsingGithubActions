080124de2fa4e07c1e483ab0a3d2293c590ab786
BoHuang2018 <tigerhuangbo@icloud.com>
2023-06-24 19:09:34 +0200

    initialized terraform remote backend

--- terraform-prod/main.tf
+++ terraform-prod/main.tf
@@ -18,9 +18,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "terraform_state_rg" # value of AZURE_RESOURCE_GROUP_FOR_TERRAFORM_STATE in ../.env
-    storage_account_name = "tfstate19015"       # value of AZURE_STORAGE_ACCOUNT_FOR_TERRAFORM_STATE in ../.env
-    container_name       = "tfstatebc"          # value of AZURE_BLOB_CONTAINER_NAME_FOR_TERRAFORM_STATE in ../.env
+    subscription_id      = "68a1244e-a9df-43d4-8bc0-07e0973ae35b"
+    resource_group_name  = "terraform_state_prod_rg" # value of AZURE_RESOURCE_GROUP_FOR_TERRAFORM_STATE in ../.env
+    storage_account_name = "tfstate19015prod"        # value of AZURE_STORAGE_ACCOUNT_FOR_TERRAFORM_STATE in ../.env
+    container_name       = "tfstatebcprod"           # value of AZURE_BLOB_CONTAINER_NAME_FOR_TERRAFORM_STATE in ../.env
     key                  = "terraform.tfstate"
     use_oidc             = true
   }
@@ -36,7 +37,3 @@ resource "azurerm_resource_group" "coop-interview-prod" {
   name     = "coop-interview-prod"
 }
 
-resource "azurerm_resource_group" "coop-interview-staging" {
-  location = "Norway East"
-  name     = "coop-interview-staging"
-}
