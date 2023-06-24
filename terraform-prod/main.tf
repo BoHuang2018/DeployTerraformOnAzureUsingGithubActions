d681311b7487f18296c4c29313a6955d046fb50d
BoHuang2018 <tigerhuangbo@icloud.com>
2023-06-24 23:06:14 +0200

    added alias to provider

--- terraform-prod/main.tf
+++ terraform-prod/main.tf
@@ -18,7 +18,7 @@ terraform {
     }
   }
   backend "azurerm" {
-#    subscription_id      = "68a1244e-a9df-43d4-8bc0-07e0973ae35b"
+    subscription_id      = "68a1244e-a9df-43d4-8bc0-07e0973ae35b"
     resource_group_name  = "terraform_state_prod_rg" # value of AZURE_RESOURCE_GROUP_FOR_TERRAFORM_STATE in ../.env
     storage_account_name = "tfstate19015prod"        # value of AZURE_STORAGE_ACCOUNT_FOR_TERRAFORM_STATE in ../.env
     container_name       = "tfstatebcprod"           # value of AZURE_BLOB_CONTAINER_NAME_FOR_TERRAFORM_STATE in ../.env
@@ -28,11 +28,13 @@ terraform {
 }
 
 provider "azurerm" {
+  alias = "prod"
   features {}
   use_oidc = true
 }
 
 resource "azurerm_resource_group" "coop-interview-prod" {
+  provider = azurerm.prod
   location = "Norway East"
   name     = "coop-interview-prod"
 }
